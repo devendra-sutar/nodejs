bed8d6f2fcf4836c1979177ced92942174fdf338
dev <sutards2001@gmail.com>
2024-06-13 07:39:56 +0000

    suny comit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 
 // Respond with "changed not" for requests that hit our root "/"
 app.get('/', function (req, res) {
-  res.send('changed not');
+  res.send('welcome dev');
 });
 
 // Listen on port 3000 by default or the port defined in the environment variables
